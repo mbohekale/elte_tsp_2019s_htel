0996f6247ebe67633eca17d9e83e9c48c4a44ad5
mbohekale <mbohekale@yahoo.com>
2019-04-28 23:10:49 +0200

    scriptFiles

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This a simple script project for hello world
+# How to get variables and using them
+# code for building and running a shell script
+# On the command line code
+# nano fileName
+# On the editor
+# First to name the file(#!/bin/bash)
+# write the code and save(exit and yes)
+# On the command line code
+# building(chmod +x fileName.sh)
+# running or excuting(./fileName.sh) 
+echo "Hello world:$(date) @ $(hostname)"
